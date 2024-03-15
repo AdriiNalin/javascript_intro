ed3fccac411fe5ad83299c0a2873f6b6c8b6d868
AdriiNalin <info.bialkowski@gmail.com>
2024-03-15 15:28:12 +0100

    first commit

---
+++ JavaScript Homework 1.js
@@ -0,0 +1,65 @@
+let lieblingsBuch = "Bodyguard"; 
+console.log(lieblingsBuch);
+
+let anzahlGeschwister = 3;
+console.log(anzahlGeschwister);
+
+let hatFuehrerschein = true;
+console.log(hatFuehrerschein);
+
+let keinFuehrerschein = false;
+console.log(keinFuehrerschein);
+
+let momentaneStimmung = null;
+console.log(momentaneStimmung);
+
+let naechsteReiseziel;
+console.log(naechsteReiseziel);
+
+let lieblingsFarbe = "Grün";
+console.log(lieblingsFarbe);
+
+let wohnflaeche = 100;
+console.log(wohnflaeche);
+
+let magKaffee = true;
+console.log(magKaffee);
+
+let magkeinKaffee = false;
+console.log(magkeinKaffee);
+
+let aktuellesProjekt = null;
+console.log(aktuellesProjekt);
+
+let letztesAbendessen = "Pizza";
+console.log(letztesAbendessen);
+
+let Geburtsstadt = "Schweinfurt";
+console.log(Geburtsstadt);
+
+let kaffeetassenProTag = 3;
+console.log(kaffeetassenProTag);
+
+let istVegetarier = false;
+console.log(istVegetarier);
+
+let zukunftsWunsch = null;
+console.log(zukunftsWunsch);
+
+let lieblingsSportart;
+console.log(lieblingsSportart);
+
+let lieblingsFilm = "Full Metal Jacket";
+console.log(lieblingsFilm);
+
+let hoeheInCM = 171;
+console.log(hoeheInCM);
+
+let hatHaustier = true;
+console.log(hatHaustier);
+
+let mittagessenIdee = null;
+console.log(mittagessenIdee);
+
+let lieblingsMusikband ="";
+console.log(lieblingsMusikband);
---
+++ hello_world.js.js
@@ -0,0 +1,1 @@
+console.log("Hello World! Life goes on");
